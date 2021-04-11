5856afaf557825548326fe3fa8ba703ec241c5bb
mu88 <4560672+mu88@users.noreply.github.com>
2021-04-11 19:59:18 +0200

    Initialize repo and create proxy

---
+++ src/JoplinAsustorMediator/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace JoplinAsustorMediator
+{
+    public class AppSettings
+    {
+        public string JoplinUrl { get; set; } = "http://192.168.178.99:1234";
+    }
+}
---
+++ src/JoplinAsustorMediator/Controllers/ProxyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AspNetCore.Proxy;
+using AspNetCore.Proxy.Options;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace JoplinAsustorMediator.Controllers
+{
+    public class ProxyController : Controller
+    {
+        private readonly HttpProxyOptions _httpOptions = HttpProxyOptionsBuilder.Instance
+            .WithShouldAddForwardedHeaders(false)
+            .WithAfterReceive(async (_, e) =>
+            {
+                if (e.StatusCode == HttpStatusCode.Forbidden) e.StatusCode = HttpStatusCode.NotFound;
+
+                if (e.StatusCode == HttpStatusCode.MultiStatus)
+                {
+                    var s = await e.Content.ReadAsStringAsync();
+                    e.Content = new StringContent(s.Replace("<D:href>", "<D:href>/joplin"));
+                }
+            }).Build();
+
+        private readonly string _joplinUrl;
+
+        public ProxyController(IOptionsMonitor<AppSettings> settings)
+        {
+            _joplinUrl = settings.CurrentValue.JoplinUrl;
+        }
+
+        [Route("{**rest}")]
+        public Task ProxyCatchAll(string rest)
+        {
+            var queryString = Request.QueryString.Value;
+            return this.HttpProxyAsync($"{_joplinUrl}/{rest}{queryString}", _httpOptions);
+        }
+    }
+}
